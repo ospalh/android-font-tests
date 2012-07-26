8687989c6e756d2cd939126a17350f4843df893f
ospalh <ospalh@gmail.com>
2012-07-26 14:04:15 +0200

    Really use the Pfenning font.

--- src/com/ospalh/boldotfdemo/BoldOTFDemo.java
+++ src/com/ospalh/boldotfdemo/BoldOTFDemo.java
@@ -15,15 +15,25 @@ public class BoldOTFDemo extends Activity
        super.onCreate(savedInstanceState);
        setContentView(R.layout.main);
 
-       Typeface demos_font = Typeface.createFromAsset(getAssets(), "fonts/Demos_LT.otf");
-       TextView m_txt = (TextView) findViewById(R.id.demos_text);  
-       TextView bm_text = (TextView) findViewById(R.id.bold_demos_text);  
-       // Typeface bold_demos_font = Typeface.create(demos_font, Typeface.BOLD);
-       Typeface bold_demos_font = Typeface.create(demos_font, 1);
-       m_txt.setTypeface(demos_font);  
-       bm_text.setTypeface(bold_demos_font);  
+       Typeface pfennig_otf_font = Typeface.createFromAsset(getAssets(), "fonts/Pfennig.otf");
+       Typeface bold_pfennig_otf_font = Typeface.create(pfennig_otf_font, Typeface.BOLD);
+       Typeface pfennig_ttf_font = Typeface.createFromAsset(getAssets(), "fonts/Pfennig.ttf");
+       Typeface bold_pfennig_ttf_font = Typeface.create(pfennig_ttf_font, Typeface.BOLD);
+
+       TextView pf_o_txt = (TextView) findViewById(R.id.pfennig_otf_text);  
+       TextView b_pf_o_txt = (TextView) findViewById(R.id.bold_pfennig_otf_text);  
+       TextView pf_t_txt = (TextView) findViewById(R.id.pfennig_ttf_text);  
+       TextView b_pf_t_txt = (TextView) findViewById(R.id.bold_pfennig_ttf_text);  
+
+       pf_o_txt.setTypeface(pfennig_otf_font);  
+       b_pf_o_txt.setTypeface(bold_pfennig_otf_font);  
+       pf_t_txt.setTypeface(pfennig_ttf_font);  
+       b_pf_t_txt.setTypeface(bold_pfennig_ttf_font);  
+
        WebView engine = (WebView) findViewById(R.id.web_engine);
        engine.loadUrl("file:///android_asset/demo.html");
+       WebView card = (WebView) findViewById(R.id.card);
+       card.loadUrl("file:///android_asset/card_demo.html");
 
     }
 }
